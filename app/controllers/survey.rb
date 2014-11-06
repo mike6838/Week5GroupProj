9888fbbed99015f0f5f2aad5ec1143623be4b138
Michael Teevan <michael.teevan@gmail.com>
2014-11-06 14:37:39 -0800

    added views

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,4 +1,6 @@
-get '/survey_show/:survey_id' do
-  @survey = Survey.all.find(params[:survey_id])
-  erb :survey_show
-end
+# just for testing
+
+# get '/survey_show/:survey_id' do
+#   @survey = Survey.all.find(params[:survey_id])
+#   erb :survey_show
+# end
