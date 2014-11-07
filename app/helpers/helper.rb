d921349c65244a410f07362fe9cb2f9d6d4b9ac5
Michael Teevan <michael.teevan@gmail.com>
2014-11-07 10:19:46 -0800

    added bcrypt

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -4,4 +4,40 @@ helpers do
      @current_user ||= User.find(session[:user_id])
     end
   end
+
+  def time_difference
+    seconds_since = (Time.now.to_i - self.created_at.to_i)
+    if seconds_since >= 86400 #seconds per day
+       days(seconds_since)
+    elsif seconds_since >= 3600 #seconds per hour
+       hours(seconds_since)
+    elsif seconds_since >= 60
+       minutes(seconds_since)
+    else
+      return "less than one minute ago"
+    end
+  end
+
+  def days(seconds_since)
+    days = seconds_since / 3600 / 24
+    display_time(days, "day")
+  end
+
+  def hours(seconds_since)
+    hours = seconds_since / 3600
+    display_time(hours, "hour")
+  end
+
+  def minutes(seconds_since)
+    minutes = seconds_since / 60
+    display_time(minutes, "minute")
+  end
+
+  def display_time(amount, time_units)
+    if amount == 1
+      "#{amount} #{time_units} ago"
+    else
+      "#{amount} #{time_units}s ago"
+    end
+  end
 end
